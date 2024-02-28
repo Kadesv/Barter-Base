56491096ffe32f79247039b6ada1d1d460cbc258
Kade Vaughan <kadevaughan6@gmail.com>
2024-02-27 21:06:24 -0700

    initial commit

--- src/Components/PostComponents/EditableText.jsx
+++ src/Components/PostComponents/EditableText.jsx
@@ -3,5 +3,4 @@ export default function EditableText() {
         <>
         </>
     )
-
 }
--- src/Components/PostComponents/PostTemplate.jsx
+++ src/Components/PostComponents/PostTemplate.jsx
@@ -1,7 +1,6 @@
 export default function ForumTemplate() {
     return (
         <>
-            
         </>
     )
 }
