660d4c526f69345a3478d3d11e6a7a015d92f482
DALLIN TANNER <dallintanner@protonmail.com>
2024-03-07 21:21:32 -0700

    worked on sign in page, the whole site shrinks with the viewport under 1000px

--- src/Pages/SignPage.jsx
+++ src/Pages/SignPage.jsx
@@ -1,11 +1,28 @@
 import SignInForm from "../Components/SignInForm"
 
 export default function SignPage() {
+
+    const handleSignIn = async (event, formData) => {
+        event.preventDefault();
+
+        const res = await axios.post('/api/auth', formData);
+        if (res.data.success) {
+            const { username } = res.data.user;
+            setUsername(username);
+            handleClose();
+            handleSignStatus();
+        } else {
+            setShow(true);
+
+
+        }
+    };
+
     return (
-        <>
+        <main>
             <h1>sign page</h1>
 
-            <SignInForm/>
-        </>
+            <SignInForm onCancel={() => onCancel()} handleSignIn={handleSignIn} />
+        </main>
     )
 }
