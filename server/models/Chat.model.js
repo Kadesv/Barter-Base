c52cf76d26301523b053d00896741079907a36c4
Kade Vaughan <kadevaughan6@gmail.com>
2024-06-20 11:31:32 -0600

    chat model has users names added to them now

--- server/models/Chat.model.js
+++ server/models/Chat.model.js
@@ -1,13 +1,11 @@
 import { DataTypes, Model } from "sequelize";
 import util from 'util';
 import { db } from "../config/db.js";
-
 export default class Chat extends Model {
   [util.inspect.custom]() {
     return this.toJSON();
   }
 }
-
 Chat.init(
   {
     chatId: {
@@ -15,15 +13,22 @@ Chat.init(
       autoIncrement: true,
       primaryKey: true,
     },
+    user1Name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     user1Id: {
       type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    user2Name: {
+      type: DataTypes.STRING,
       allowNull: false,
     },
     user2Id: {
       type: DataTypes.INTEGER,
       allowNull: false,
     }
-
   },
   {
     modelName: 'chat',
