98e72f8298a9b1344e9371008d07bdc5681f5544
Kade Vaughan <kadevaughan6@gmail.com>
2024-11-06 21:14:52 -0700

    removed the handle load function and added tooltip functionality to category and subcategory badges

--- src/Components/PostCard.jsx
+++ src/Components/PostCard.jsx
@@ -9,7 +9,7 @@ const currencyFormat = new Intl.NumberFormat("en-US", {
 });
 export function PostCard({ post, categories, favorites, authUser, setFavorites }) {
     const navigate = useNavigate();
-    const [isWide, setIsWide] = useState(false);
+    // const [isWide, setIsWide] = useState(false);
     const [imageLoaded, setImageLoaded] = useState(false);
     const backupLink = 'https://firebasestorage.googleapis.com/v0/b/mytradingproject-6.appspot.com/o/posts%2FYour%20paragraph%20text%20(1).png?alt=media&token=776ac434-702f-456a-b0f0-15eb6f388e1a';
 
@@ -29,16 +29,6 @@ export function PostCard({ post, categories, favorites, authUser, setFavorites }
         }
     };
 
-    const handleImageLoad = (e) => {
-        e.preventDefault();
-        setImageLoaded(true);
-        const img = e.target
-        console.log(e)
-        if (img.naturalHeight < img.naturalWidth) {
-            setIsWide(true);
-        }
-    }
-
     const showModal = (e) => {
         if (e.target.tagName !== 'BUTTON' && e.target.tagName !== 'svg') {
             document.getElementById(`model-popup${post.postId}`).showModal();
@@ -57,19 +47,19 @@ export function PostCard({ post, categories, favorites, authUser, setFavorites }
                         src={post.image[0]}
                         alt='IMAGE'
                         draggable={false}
-                        onLoad={(e) => handleImageLoad(e)}
+                        onLoad={() => setImageLoaded(true)}
                         onError={(e) => e.target.src !== backupLink ? e.target.src = backupLink : ''}
-                        className={`${imageLoaded ? '' : 'skeleton h-full'} w-full border-b-4 border-base-300`}
+                        className={`${imageLoaded ? '' : 'skeleton h-96 w-full'} w-full border-b-4 border-base-300`}
                     />
                 </figure>
             
                 <div className='flex flex-col w-full justify-between px-2 m-1'>
                     <div className='flex flex-col justify-between'>
                         <div className='flex flex-row min-w-0 gap-1'>
-                            <span className='badge bg-transparent border-transparent badge-xs'>
+                            <span data-tip="Sub-Category" className='badge lg:tooltip lg:tooltip-bottom bg-transparent border-transparent badge-xs'>
                                 {categories.find(cat => cat.categoryId === post.categoryId).categoryName}
                             </span>
-                            <span className='badge bg-transparent border-transparent badge-xs'>
+                            <span data-tip="Sub-Category" className='badge lg:tooltip lg: tooltip-bottom bg-transparent border-transparent badge-xs'>
                                 {categories.find(cat => cat.categoryId === post.categoryId).subcategories.find(sub => sub.subCategoryId === post.subCategoryId).subCategoryName}
                             </span>
                         </div>
